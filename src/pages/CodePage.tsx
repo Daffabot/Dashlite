18ad70327ce22481803b4f581b0558d0baa945d8
daffabot <daffabot@programmer.net>
2025-10-24 10:13:50 +0700

    chore: text codepage

--- src/pages/CodePage.tsx
+++ src/pages/CodePage.tsx
@@ -11,7 +11,7 @@ const CodePage: React.FC<CodePageProps> = ({ codeProps }) => {
     <div className="flex items-center justify-center h-screen w-screen bg-lightblue-dashlite/80 px-6">
       <Card className="text-center space-y-6 max-w-md w-full">
         <div>
-          <h1 className="text-9xl font-bold text-blue-600">{codeProps.code}</h1>
+          <h1 className="text-9xl font-bold text-lightblue-dashlite">{codeProps.code}</h1>
           <p className="text-xl font-semibold mt-2">{codeProps.title}</p>
           <p className="text-base text-gray-500 font-bold mt-1">
             {codeProps.desc}
